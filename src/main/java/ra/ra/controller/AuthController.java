6de8b50f47d3c3e8f174a646bbe934811d413e2f
tuan_anh <119550348+tuananhafjdjhv@users.noreply.github.com>
2023-06-05 10:34:09 +0700

    ra đê

--- src/main/java/ra/ra/controller/AuthController.java
+++ src/main/java/ra/ra/controller/AuthController.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ra.ra.model.User;
 
 @Controller
+@RequestMapping({"/"})
 public class AuthController {
-    @GetMapping("/login")
+    @GetMapping({"/"})
     public ModelAndView showLoginForm() {
         ModelAndView modelAndView = new ModelAndView("login");
         modelAndView.addObject("user", new User());
